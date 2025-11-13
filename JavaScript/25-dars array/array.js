56a3c871fd84939e3277932b9ed3e97c040ce892
Jahongir Omonboyev <omonboyevjoxa50@gmail.com>
2025-11-13 19:58:28 +0500

    Birinchi commit

---
+++ JavaScript/25-dars array/array.js
@@ -0,0 +1,18 @@
+let names = ["Jahongir", "Alijon", "Valijon", "Hasan"];
+
+//console.log(names);
+//names[1] = "User";
+//console.log(names);
+
+//! => push => oxiriga qo'shish
+
+names.push("Amir");
+
+//! => unshift => boshiga qiymat qo'shish
+names.unshift("James");
+
+//! => shift => boshidagi qiymatni o'chirish
+names.shift
+
+
+console.log(names);
---
+++ JavaScript/25-dars array/task.js
@@ -0,0 +1,13 @@
+let numbers = [25, 36,]
+
+let a = 25;
+let b = 36;
+
+if (a > b) {
+    console.log(a + "katta");
+    
+}  else if (a < b) {
+    console.log(b + " katta");
+}  
+
+ 
