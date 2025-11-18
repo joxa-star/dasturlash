365ea85d48b7597dadbe8e54bc78500230ecab1e
Jahongir Omonboyev <omonboyevjoxa50@gmail.com>
2025-11-18 16:33:18 +0500

    Birinchi commit

---
+++ JavaScript/27-DARS while/for.js
@@ -0,0 +1,17 @@
+// let numbers = 0;
+// for (let i = 10; i < 90; i++ ) {
+//     numbers += i
+// }
+// console.log(numbers);
+
+
+let names = ["Shuhruz", "Guli", "Anaxon", "Shahnoza", "Zulayho", "Dilyor", "Behruz",
+    "Shaxzod", "Alixon"];
+
+for (let i = 0; i < names.length; i++) {
+    if (names[i] != "Alixon" ) {
+        console.log(names[i] + "bek");
+        
+    }
+}
+
