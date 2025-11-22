ebf2be0a30538cdc992bcc8e1d2bd4c83882d884
Jahongir Omonboyev <omonboyevjoxa50@gmail.com>
2025-11-22 13:54:29 +0500

    birinchi commit

---
+++ JavaScript/26-dars loop/loop.js
@@ -0,0 +1,19 @@
+//! for-3
+
+// for (let i = 2; i <= 8; i ++) {
+//    console.log(i);
+// }
+
+//! for-6
+let kg = 18;
+for (let i = 1.2; i <= 2; i += 0.2 ) {
+    console.log((i * kg).toFixed(1) + " kg");
+    
+}
+
+
+
+
+
+
+
